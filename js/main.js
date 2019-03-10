fe111cc277a5667fdd67f43bd6f4ba02b52b14c2
Ruslan ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2019-03-10 17:32:38 +0600

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function() {
+
+    // inner variables
+    var song;
+    var tracker = $('.tracker');
+    var volume = $('.volume');
+
+    function initAudio(elem) {
+        var url = elem.attr('audiourl');
+        var title = elem.text();
+        var cover = elem.attr('cover');
+        var artist = elem.attr('artist');
+
+        $('.player .title').text(title);
+        $('.player .artist').text(artist);
+        $('.player .cover').css('background-image','url(data/' + cover+')');;
+
+        song = new Audio('data/' + url);
+
+        // timeupdate event listener
+        song.addEventListener('timeupdate',function (){
+            var curtime = parseInt(song.currentTime, 10);
+            tracker.slider('value', curtime);
+        });
+
+        $('.playlist li').removeClass('active');
+        elem.addClass('active');
+    }
+    function playAudio() {
+        song.play();
+
+        tracker.slider("option", "max", song.duration);
+
+        $('.play').addClass('hidden');
+        $('.pause').addClass('visible');
+    }
